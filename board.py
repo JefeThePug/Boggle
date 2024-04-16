e3cd0a5d4cdc21189102f87e97b4605aa14feb49
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2024-04-16 16:45:52 +0900

    move letter_gen from the class to the helper.py file

--- board.py
+++ board.py
@@ -1,16 +1,15 @@
 import numpy as np
 
-from random import choice, randint
 from rich.console import Console
 from rich.style import Style
 from rich.table import Table
 
-from helper import is_valid
+from helper import is_valid, letter_gen
 
 
 class Board:
     def __init__(self):
-        self.letters = np.array([*Board.letter_gen()], dtype="<U2").reshape(4, 4)
+        self.letters = np.array([*letter_gen()], dtype="<U2").reshape(4, 4)
 
         # Display
         self.style = Style(color="black", bgcolor="navajo_white1", bold=1)
@@ -27,18 +26,6 @@ class Board:
             "border_style": "black",
         }
         self.console = Console()
-
-    @staticmethod
-    def letter_gen():
-        DICE = [
-            "RIFOBX", "IFEHEY", "DENOWS", "UTOKND",
-            "HMSRAO", "LUPETS", "ACITOA", "YLGKUE",
-            "QBMJOA", "EHISPN", "VETIGN", "BALIYT",
-            "EZAVND", "RALESC", "UWILRG", "PACEMD",
-        ]
-        while DICE:
-            die = DICE.pop(randint(0, len(DICE) - 1))
-            yield choice(die)
 
     def is_adjacent(self, word, used, row, col, i):
         if i == len(word):
@@ -75,6 +62,7 @@ class Board:
 
     def display(self):
         letters = np.where(self.letters == "Q", "Qu", self.letters)
+        
         table = Table(**self.table)
         for _ in range(4):
             table.add_column("   ", **self.buttons)
--- helper.py
+++ helper.py
@@ -1,7 +1,20 @@
 import requests
+
 from lxml import html
+from random import choice, randint
 
 def is_valid(word):
     url = f"http://wordnetweb.princeton.edu/perl/webwn?s={word}"
     request = requests.get(url)
     return bool(html.fromstring(request.content).xpath("//div[@class='key']"))
+
+def letter_gen():
+    DICE = [
+        "RIFOBX", "IFEHEY", "DENOWS", "UTOKND",
+        "HMSRAO", "LUPETS", "ACITOA", "YLGKUE",
+        "QBMJOA", "EHISPN", "VETIGN", "BALIYT",
+        "EZAVND", "RALESC", "UWILRG", "PACEMD",
+    ]
+    while DICE:
+        die = DICE.pop(randint(0, len(DICE) - 1))
+        yield choice(die)
